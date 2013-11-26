106fd43326180c0376553153d98ae01625c58104
gabanjo <bataleck@gmail.com>
2013-11-26 17:27:11 +0000

    working...

--- lane.pl
+++ lane.pl
@@ -5,10 +5,12 @@ use XML::Parser;
 use Encode;
 use utf8;
 use DBI;
+use File::Find;
+
 my $missingIdCount = 0;
 binmode STDERR, ":utf8";
 binmode STDOUT, ":utf8";
-my $blog;               # buckwalter conversion log handle
+my $blog;                       # buckwalter conversion log handle
 
 sub writelog {
   my $h = shift;
@@ -40,8 +42,8 @@ sub convertString {
   $c += ($t =~ tr/^=_/\x{654}\x{622}\x{640}/);
 
   # count the spaces etc
-
-  my $spaces = ($t =~ s/\s/\s/g);
+  my $r = $t;
+  my $spaces = ($r =~ s/ / /g);
 
   if (($c + $spaces) != length $t) {
     writelog($blog,"conversion error [$s]->[$t]");
@@ -62,7 +64,7 @@ sub processRoot {
   my $keyAttr;
 
 
-  for(my $i=0;$i < $entryCount;$i++) {
+  for (my $i=0;$i < $entryCount;$i++) {
     my $entry = $entries->item($i);
     my $id;
     my $key;
@@ -71,8 +73,7 @@ sub processRoot {
     $idAttr = $entry->getAttributeNode("id");
     if ($idAttr) {
       $id = $idAttr->getValue();
-    }
-    else {
+    } else {
       print STDERR "Node has no ID:" . $entry->toString . "\n";
       $missingIdCount++;
     }
@@ -84,7 +85,7 @@ sub processRoot {
 
     if ($id && $key) {
       print "\t$id\t\t$key\t$ar_key\n";
-
+      $entry->setAttribute("key",$ar_key);
     }
   }
 
@@ -93,41 +94,74 @@ sub parseFile {
 
   my $fileName = shift;
 
- my $parser = new XML::DOM::Parser;
- my $doc = $parser->parsefile ($fileName);
+  my $parser = new XML::DOM::Parser;
+  my $doc = $parser->parsefile ($fileName);
 
- # print all HREF attributes of all CODEBASE elements
- my $nodes = $doc->getElementsByTagName ("div1");
- my $n = $nodes->getLength;
+  # print all HREF attributes of all CODEBASE elements
+  my $nodes = $doc->getElementsByTagName ("div1");
+  my $n = $nodes->getLength;
 
-for (my $i = 0; $i < $n; $i++) {
+  for (my $i = 0; $i < $n; $i++) {
 
-     my $div1Node = $nodes->item($i);
-     my $roots = $div1Node->getElementsByTagName("div2");
-     my $rootCount = $roots->getLength;
-     for (my $j=0;$j < $rootCount;$j++) {
-       my $div2Node = $roots->item($j);
-       if ($div2Node->getAttributeNode("type")->getValue() eq "root") {
-         print $div2Node->getAttributeNode("n")->getValue() . "\n";
-         processRoot($div2Node);
+    my $div1Node = $nodes->item($i);
+    my $roots = $div1Node->getElementsByTagName("div2");
+    my $rootCount = $roots->getLength;
+    for (my $j=0;$j < $rootCount;$j++) {
+      my $div2Node = $roots->item($j);
+      if ($div2Node->getAttributeNode("type")->getValue() eq "root") {
+        print $div2Node->getAttributeNode("n")->getValue() . "\n";
+        processRoot($div2Node);
 
-       }
-     }
- }
-print "Missing ID count:$missingIdCount\n";
+      }
+    }
+  }
+  print "Missing ID count:$missingIdCount\n";
 
 
- # Print doc file
-# $doc->printToFile ("out.xml");
+  # Print doc file
+  # $doc->printToFile ("out.xml");
 
- # Print to string
- #print $doc->toString;
+  # Print to string
+  #print $doc->toString;
 
- # Avoid memory leaks - cleanup circular references for garbage collection
- $doc->dispose;
+  # Avoid memory leaks - cleanup circular references for garbage collection
+  my $str =  $doc->toString;
+
+  open(OUT,">:encoding(UTF8)","/tmp/test.xml");
+#  binmode OUT, ":utf8";
+  print OUT $str;
+
+  close OUT;
+  $doc->dispose;
 }
+sub parseDirectory {
+  my $d = shift;
 
+  if (! -d $d ) {
+    print STDERR "No such directory:$d\n";
+    return;
+
+  }
+  eval {
+    my @arr;
+    find sub { if ((-f $_) && ($File::Find::name =~ /xml$/))
+                 {
+                   push @arr,$File::Find::name;
+                 }
+             }, $d;
+    foreach my $file (@arr) {
+      print "$file\n";
+    }
+
+  };
+  if ($@) {
+    print STDERR "Error opening directory $d\n";
+
+  }
+}
 #convertString("ja Oxdr sthwmn");
 open $blog,">x.log";
 
-parseFile("./xml/j0.xml");
+#parseFile("./xml/j0.xml");
+parseFile("./test/test_j0.xml");
+#parseDirectory("./xml");
