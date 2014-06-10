25f9f4d5bd81659f2d670e75a96de54bd1b6dc88
Graeme Andrews <graeme3317@gmail.com>
2014-06-10 11:19:26 +0100

    moved code from check.pl

--- stats.pl
+++ stats.pl
@@ -4,6 +4,11 @@ use warnings;
 use File::Find;
 use English;
 use FileHandle;
+use DBI;
+use XML::LibXML;
+my $dbh;
+binmode STDERR, ":encoding(UTF-8)";
+binmode STDOUT, ":encoding(UTF-8)";
 sub convertString {
   my $t = shift;
   my $s = $t;
@@ -63,10 +68,39 @@ sub convertString {
 
   return $t;
 }
+sub getVolForPage {
+  my $page = shift;
+
+  if ($page < 368) {
+    return 1;
+  }
+  if ($page < 838) {
+    return 2;
+  }
+  if ($page < 1281) {
+    return 3;
+  }
+  if ($page < 1758) {
+    return 4;
+  }
+  if ($page < 2220) {
+    return 5;
+  }
+  if ($page < 2476) {
+    return 6;
+  }
+  if ($page < 2750) {
+    return 7;
+  }
+  if ($page < 3065) {
+    return 8;
+  }
+  return -1;
+}
 ##################################################################################
 # return list of log files in the given directory
 ###################################################################################
-sub readLogDirectory {
+sub readDirectory {
   my $d = shift;
   my $pattern = shift;
   my @arr;
@@ -223,7 +257,7 @@ sub summaryStats {
   my ($fh,$outf);
   my ($day,$month,$year) = (localtime)[3,4,5];
   my $rundate = sprintf "%04d-%02d-%02d",$year+1900,$month+1,$day;
-  my @convfile =  readLogDirectory("/home/andrewsg/parse",$pattern);
+  my @convfile =  readDirectory("/home/andrewsg/parse",$pattern);
   format SUMMARY_TOP =
    @<<<<<<<<<                           Error Summary                              Page @<<
 $rundate,$%
@@ -283,6 +317,139 @@ $filename,$type2,$type3,$type4,$type5,$type6,$typeother
 
  }
 }
+#############################################
+# prints out double question marks
+#############################################
+sub get_dqs {
+  my $f = shift;
+  my $dqh = shift;
+  my $sth = shift;
+  my $parser = XML::LibXML->new;
+  $parser->set_options("line_numbers" => "parser");
+  #  my $parser = new XML::DOM::Parser;
+  my $doc = $parser->parse_file($f);
+  $doc->setEncoding("UTF-8");
+  my $nodes = $doc->getElementsByTagName ("entryFree");
+  my $n = $nodes->size();
+  my ($id,$word,$pos,$nodeId);
+  my $margin = 60;
+  my $totalCount = 0;
+  my $page;
+  for (my $i=0;$i < $n;$i++) {
+    my $node = $nodes->item($i);
+    if ($node->textContent && ($node->textContent =~ /see\s+supplement/i)) {
+      next;
+    }
+    my $keyAttr = $node->getAttributeNode("key");
 
+    if ($keyAttr) {
+      $word = convertString($keyAttr->value);
+    }
+    else {
+      $word = "";
+    }
+    $id = $node->getAttributeNode("id");
+    if ($id) {
+      $nodeId = $id->value;
+      if ($sth) {
+        $sth->bind_param(1,$nodeId)  ;
+        $sth->execute();
+        $page = $sth->fetchrow_array;
+      }
+    } else {
+      $nodeId = "<NO NODEID>";
+      my $t = $node->textContent;
+      my @kids = $node->nonBlankChildNodes();
+      my $form = $kids[0];
+      if ($form) {
+        my @gkids = $form->nonBlankChildNodes();
+        my $itype = $gkids[0];
+        if ($itype) {
+          $t = $itype->nodeName;
+        }
+      }
+      if ($t ne "itype") {
+        print $dqh sprintf "WARNING %6d No node id (%s), V%s/%s\n",$node->line_number(),$t,getVolForPage($page),$page;
+      }
+    }
+
+    my $txt = $node->toString();
+
+    my $count = 0;
+    while($txt =~ /(\?\?)/g) {
+      $count++;
+      if ($count == 1) {
+        print $dqh sprintf "%-6d %8s %s V%s/%s\n",$node->line_number(),$nodeId , $word,getVolForPage($page),$page;
+      }
+      $pos =  pos($txt) - length $1;
+      my $start = 0;
+      my $end = length $txt;
+      if ($pos > $margin) {
+        $start = $pos - $margin;
+      }
+      if (($pos + (length $1) + $margin) < $end) {
+        $end = $pos + (length $1) + $margin;
+      }
+      print $dqh substr $txt,$start, $end - $start;
+      print $dqh "\n";
+    }
+    $totalCount += $count;
+  }
+}
+################################################################
+#
+#
+################################################################
+sub openDb {
+  my $db = shift;
+
+  my $sth;
+  ### Attributes to pass to DBI->connect(  )
+  my %attr = (
+              PrintError => 1,
+              RaiseError => 0
+             );
+  eval {
+    $dbh = DBI->connect("dbi:SQLite:$db","","",\%attr) or die "couldn’t connect to db" . DBI->errstr;
+  };
+  if ($@) {
+    print STDERR "Error opening db:$@\n";
+    exit 1;
+  } else {
+
+  }
+  $dbh->{AutoCommit} = 1;
+}
+
+######################################################
+#
+#  looks for ?? in XML
+#
+######################################################
+sub check_double_questions {
+  my $xmldir = shift;
+  my $dbname = shift;
+  my $sth;
+  my @xml = readDirectory($xmldir,"xml\$");
+
+  my $dqh;
+  open $dqh,">:encoding(UTF8)","double_questions.txt" or die $@;
+  if ($dbname) {
+    openDb($dbname);
+    if ($dbh) {
+      $sth = $dbh->prepare("select page from entry where nodeId = ?");
+    }
+  }
+  foreach my $f (@xml) {
+      print $dqh "\n$f\n";
+      for(my $i=0;$i < length $f;$i++) {
+        print $dqh "=";
+      }
+      print $dqh "\n";
+      get_dqs($f,$dqh,$sth);
+  }
+   close $dqh;
+}
+check_double_questions("./xml_originals","lexicon.sqlite");
 summaryStats("lexicon","/home/andrewsg/parse");
 convErrors("lexicon","/home/andrewsg/parse");
