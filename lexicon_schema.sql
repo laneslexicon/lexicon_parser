7f10d5e685aa3926fe85dfe2d9eaf787c90ebaef
Graeme Andrews <graeme3317@gmail.com>
2014-10-23 10:30:17 +0100

    added 'headword' to entry

--- lexicon_schema.sql
+++ lexicon_schema.sql
@@ -1,9 +1,10 @@
 CREATE TABLE root (id integer primary key,datasource integer,word text,bword text,letter text,bletter text,xml text,supplement integer,quasi integer,alternates integer,page integer);
 CREATE TABLE alternate (id integer primary key,datasource integer,word text,bword text,letter text,bletter text,xml text,supplement integer,quasi integer,alternate integer);
 CREATE TABLE itype (id integer primary key,datasource integer,itype integer,root text,broot text,nodeId text,word text,xml text, bareword text);
-CREATE TABLE entry (id integer primary key,datasource integer,root text,broot text,word text,bword text,itype text,nodeId text,xml text,supplement integer,file text,page integer,nodenum real, bareword text,perseusxml text);
+CREATE TABLE entry (id integer primary key,datasource integer,root text,broot text,word text,bword text,itype text,nodeId text,xml text,supplement integer,file text,page integer,nodenum real, bareword text,perseusxml text,headword text);
 CREATE TABLE orth (id integer primary key, datasource integer,entryid integer,form text,bform text,nodeid text,root text,broot text);
 CREATE INDEX 'word_index' on entry (word asc);
+CREATE INDEX 'headword_index' on entry (headword asc);
 CREATE INDEX 'broot_index' on entry (broot asc);
 CREATE INDEX 'root_index' on entry (root asc);
 CREATE TABLE xref (id INTEGER primary key,datasource integer,word TEXT,bword text,node TEXT,type INTEGER,page integer, bentry text, entry text, broot text, root text, bareword text,nodenum real);
