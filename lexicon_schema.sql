1da851f45539cc3f6bbe133d3f120dea2b7bb5e9
laneslexicon <laneslexicon@theunwalledcity.com>
2015-11-18 08:26:48 +0000

    links table changed for new <orth> handling

--- lexicon_schema.sql
+++ lexicon_schema.sql
@@ -2,7 +2,11 @@ CREATE TABLE root (id integer primary key,datasource integer,word text,bword tex
 CREATE TABLE alternate (id integer primary key,datasource integer,word text,bword text,letter text,bletter text,xml text,supplement integer,quasi integer,alternate integer);
 CREATE TABLE itype (id integer primary key,datasource integer,itype integer,root text,broot text,nodeid text,word text,xml text, bareword text);
 CREATE TABLE entry (id integer primary key,datasource integer,root text,broot text,word text,bword text,itype text,nodeid text,xml text,supplement integer,file text,page integer,nodenum real, bareword text,perseusxml text,headword text, type int default 0);
-CREATE TABLE links (id integer primary key, linkid integer,datasource integer,root text,word text,fromnode text,tonode text,link text,matchtype integer,matchedword text,orthtype integer default -1,status integer default -1);
+CREATE TABLE links (id integer primary key, linkid integer,datasource integer,root text,word text,fromnode text,tonode text,link text,matchtype integer,matchedword text,orthfixtype integer default -1,orthpattern text,orthindex integer,status integer default -1,note text);
+CREATE INDEX 'fromnode_link' on links (fromnode asc);
+CREATE INDEX 'status_index' on links (status asc);
+CREATE INDEX 'linkid_index' on links (linkid asc);
+
 CREATE INDEX 'word_index' on entry (word asc);
 CREATE INDEX 'headword_index' on entry (headword asc);
 CREATE INDEX 'broot_index' on entry (broot asc);
@@ -20,5 +24,5 @@ CREATE INDEX 'xref_word' on xref (word asc);
 CREATE INDEX 'xref_nodenum' on xref (nodenum asc);
 CREATE INDEX 'bareword_index' on entry (bareword asc);
 CREATE INDEX page_index on entry (page asc);
-CREATE INDEX linkid_index on links (linkid asc);
+
 CREATE TABLE lexicon (id integer primary key, sourceid integer,description text,createversion text,createdate text,updateversion text,updatedate text,xmlversion text,dbid text);
